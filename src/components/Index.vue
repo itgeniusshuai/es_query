7e3a2709bf9c3a22327392b58c2d5d93e0830940
itgeniusshuai <1115870494@qq.com>
2018-06-18 02:09:11 +0800

    修改稿样式

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -6,6 +6,18 @@
       <Left></Left>
       <Main></Main>
     </div>
+    <div id="connDiv">
+      <el-dialog title="新建连接" :visible.sync="connDialogVisible" custom-class="connDialog" width="400px">
+        <div>
+
+        </div>
+        <div slot="footer" class="dialog-footer">
+          <el-button @click="connDialogVisible = false" size="small" class="btn">取 消</el-button>
+          <el-button type="primary" @click="connDialogVisible = false" plain size="small" class="btn">确 定</el-button>
+        </div>
+      </el-dialog>
+    </div>
+
   </div>
 </template>
 
@@ -21,6 +33,18 @@ export default {
   name: 'Index',
   data () {
     return {
+      connDialogVisible:false,
+      form: {
+          name: '',
+          region: '',
+          date1: '',
+          date2: '',
+          delivery: false,
+          type: [],
+          resource: '',
+          desc: ''
+        },
+      formLabelWidth: '120px',
       itemList: [
         {"itemImageUrl":require("./../assets/conn.png"),"itemName":"连接","id":"conn"},
         {"itemImageUrl":require("./../assets/doc.png"),"itemName":"文档","id":"doc"},
@@ -34,7 +58,6 @@ export default {
     clickItemIndex(id){
       switch(id){
         case 'conn':
-          console.log(this)
           this.clickConn();
           break;
         case 'search':
@@ -47,19 +70,28 @@ export default {
     },
     clickSearch(){
       this.$alert('search111');
-
     },
     clickDoc(){
       this.$alert("doc111");
     },
     clickConn(){
-      this.$alert("conn111");
+      this.connDialogVisible = true
     }
   },
   components:{Nav,Left,Main}
 }
 </script>
 
-<style scoped>
-
+<style>
+  #connDiv.connDialog{
+    font-size: 12px;
+    height: 300px;
+    width: 400px;
+  }
+  #connDiv .btn{
+    width: 100px;
+  }
+  #connDiv .el-dialog__title {
+    font-size:12px!important;
+    }
 </style>
