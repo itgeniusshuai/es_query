9aba7eecf63396a4893b30f27d4e51673f7789af
itgeniusshuai <1115870494@qq.com>
2018-06-29 16:09:22 +0800

    删除tab功能

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="main">
-    <el-tabs v-model="editableTabsValue" type="card" closable @tab-click="tabClick">
+    <el-tabs v-model="editableTabsValue" type="card" closable @tab-click="tabClick" @edit="handleTabsEdit">
       <el-tab-pane
         :key="item.name"
         v-for="(item) in editableTabs"
@@ -148,7 +148,26 @@ export default {
     },
     tabClick(targetName){
       this.currentItems = this.items[targetName.name]
-    }     
+    },
+    handleTabsEdit(targetName, action){
+      if (action === 'remove') {
+          let tabs = this.editableTabs;
+          let activeName = this.editableTabsValue;
+          if (activeName === targetName) {
+            tabs.forEach((tab, index) => {
+              if (tab.name === targetName) {
+                let nextTab = tabs[index + 1] || tabs[index - 1];
+                if (nextTab) {
+                  activeName = nextTab.name;
+                }
+              }
+            });
+          }
+          
+          this.editableTabsValue = activeName;
+          this.editableTabs = tabs.filter(tab => tab.name !== targetName);
+      }
+    }   
   }
 }
 </script>
