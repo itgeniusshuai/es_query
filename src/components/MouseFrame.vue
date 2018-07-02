549f3ca7f03de2e196e44594d77e7777ec8ebff9
itgeniusshuai <1115870494@qq.com>
2018-07-02 12:43:37 +0800

    增加蒙板取消默认右键

--- src/components/MouseFrame.vue
+++ src/components/MouseFrame.vue
@@ -1,12 +1,14 @@
 <template>
-  <div class="mouse" :style="{left:menuLeft+'px',top:menuTop+'px'}" v-show="isShowMenu">
-      <a class="btn" >删除</a>
-      <a class="btn" >添加</a>
-      <div class="hr"/>
-      <a class="btn" >删除</a>
-      <a class="btn" >添加</a>
-      <div class="hr"/>
-      <a class="btn">其他</a>
+  <div class="mouseBorad" v-show="isShowMenu" @contextmenu.prevent="hiddenRightMouse" @click="hiddenRightMouse">
+    <div class="mouse" :style="{left:menuLeft+'px',top:menuTop+'px'}" v-show="isShowMenu">
+        <a class="btn" >删除</a>
+        <a class="btn" >添加</a>
+        <div class="hr"/>
+        <a class="btn" >删除</a>
+        <a class="btn" >添加</a>
+        <div class="hr"/>
+        <a class="btn">其他</a>
+    </div>
   </div>
 </template> 
 
@@ -19,13 +21,22 @@ export default {
     }
   },
   methods:{
-   
+   hiddenRightMouse(){
+     this.isShowMenu = false;
+   }
   },
   props:["isShowMenu","menuLeft","menuTop"]
 }
 </script>
 
 <style scoped>
+  .mouseBorad{
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top:0;
+  }
   .mouse{
     width: 130px;
     position: absolute;
@@ -45,7 +56,7 @@ export default {
     overflow: hidden;
   }
   .btn:hover{
-    background-color: aqua;
+    background-color: #0ff;
   }
   .hr{
     border-top: 2px solid gray;
